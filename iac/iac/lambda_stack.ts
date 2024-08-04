489e7dc2758375370f2b2f2e89aa4aa2d4561731
lucapinheiro <lucapgomes11@gmail.com>
2024-08-04 01:31:35 -0300

    refactor: Update LambdaStack to use environment variables for configuration

--- iac/iac/lambda_stack.ts
+++ iac/iac/lambda_stack.ts
@@ -1,55 +1,46 @@
 import { Construct } from "constructs";
-import * as lambda from "aws-cdk-lib/aws-lambda";
-import path from "path";
+import { envs } from "../../src/envs";
+import { stage } from "../get_stage_app";
+import * as lambda from "aws-cdk-lib/aws-lambda"
+import path from "path"
 import { CfnOutput, Duration } from "aws-cdk-lib";
 
+const stackName = envs.STACK_NAME
+const EMAIL_LOGIN = envs.EMAIL_LOGIN
+const EMAIL_PASSWORD = envs.EMAIL_PASSWORD
+
 export class LambdaStack extends Construct {
-  lambdaHandler: lambda.Function;
-  nodeModulesLayer: lambda.LayerVersion;
+  lambdaHandler: lambda.Function
+  nodeModulesLayer: lambda.LayerVersion
 
   constructor(scope: Construct, environmentVariables: Record<string, string>) {
-    super(
-      scope,
-      `${environmentVariables.STACK_NAME}-${environmentVariables.STAGE}`
-    );
+    super(scope, `${stackName}-${stage}`)
 
-    this.nodeModulesLayer = new lambda.LayerVersion(
-      this,
-      `${environmentVariables.STACK_NAME}-${environmentVariables.STAGE}-node-modules-Layer`,
-      {
-        code: lambda.Code.fromAsset(path.join(__dirname, "../dependencies")),
-        compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
-        description:
-          "Node modules layer to use deps in lambda functions for APPROLE Mail Service",
-      }
-    );
+    this.nodeModulesLayer = new lambda.LayerVersion(this, `${stackName}-${stage}-node-modules-Layer`, {
+      code: lambda.Code.fromAsset(path.join(__dirname, "../dependencies")),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
+      description: "Node modules layer to use deps in lambda functions for APPROLE Mail Service",
+    })
 
-    this.lambdaHandler = new lambda.Function(
-      this,
-      `${environmentVariables.STACK_NAME}-${environmentVariables.STAGE}-lambda-handler`,
-      {
-        functionName: `${environmentVariables.STACK_NAME}-${environmentVariables.STAGE}-lambda-handler`,
-        code: lambda.Code.fromAsset(path.join(__dirname, "../../build")),
-        handler: "server.handler",
-        runtime: lambda.Runtime.NODEJS_20_X,
-        environment: environmentVariables,
-        layers: [this.nodeModulesLayer],
-        timeout: Duration.seconds(30),
-        memorySize: 256,
-      }
-    );
+    this.lambdaHandler = new lambda.Function(this, `${stackName}-${stage}-lambda-handler`, {
+      functionName: `${stackName}-${stage}-lambda-handler`,
+      code: lambda.Code.fromAsset(path.join(__dirname, "../../build")),
+      handler: "server.handler",
+      runtime: lambda.Runtime.NODEJS_20_X,
+      environment: environmentVariables,
+      layers: [this.nodeModulesLayer],
+      timeout: Duration.seconds(30),
+      memorySize: 256,
+    })
 
     const lambdaUrl = this.lambdaHandler.addFunctionUrl({
-      authType: lambda.FunctionUrlAuthType.NONE,
-    });
+      authType: lambda.FunctionUrlAuthType.NONE
+    })
 
-    new CfnOutput(
-      this,
-      `${environmentVariables.STACK_NAME}-${environmentVariables.STAGE}-lambda-url`,
-      {
-        value: lambdaUrl.url,
-        description: "The URL to access the lambda function",
-      }
-    );
+    new CfnOutput(this, `${stackName}-${stage}-lambda-url`, {
+      value: lambdaUrl.url,
+      description: "The URL to access the lambda function"
+    })
+
   }
 }
