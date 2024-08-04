b8e339e7cd8f0edfabd494967f4f6a2f79d13442
lucapinheiro <lucapgomes11@gmail.com>
2024-08-04 01:43:50 -0300

    refactor: Update router.ts to use consistent syntax for route handlers

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { healthCheckHandler } from "../handlers/health_check_handler";
 import { emailHandler } from "../handlers/mail_handler";
 
-export const router = Router()
+export const router = Router();
 
-router.get("/health", healthCheckHandler)
-router.post("/send-email", emailHandler)
+router.get("/health", healthCheckHandler);
+router.post("/send-email", emailHandler);
