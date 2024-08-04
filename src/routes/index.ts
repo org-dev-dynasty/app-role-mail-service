8656f2d4c67e92fe90c41dfec9d14c0e73a54e4f
lucapinheiro <lucapgomes11@gmail.com>
2024-08-04 00:56:55 -0300

    feat: Add emailHandler route for sending emails

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { healthCheckHandler } from "../handlers/health_check_handler";
+import { emailHandler } from "../handlers/mail_handler";
 
 export const router = Router()
 
-router.get("/health", healthCheckHandler) 
+router.get("/health", healthCheckHandler)
+router.post("/send-email", emailHandler)
