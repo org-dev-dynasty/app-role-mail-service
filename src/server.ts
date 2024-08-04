780baac2d97bb5c692664e4d851eee48ee871ca1
lucapinheiro <lucapgomes11@gmail.com>
2024-08-04 01:43:47 -0300

    refactor: Update server.ts to use express middleware consistently

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import express, { Request, Response } from "express"
-import { envs } from "./envs"
-import { router } from "./routes"
-import { STAGE } from "./enums/stage_enum"
-import ServerlessHttp from "serverless-http"
+import express, { Request, Response } from "express";
+import { envs } from "./envs";
+import { router } from "./routes";
+import { STAGE } from "./enums/stage_enum";
+import ServerlessHttp from "serverless-http";
 
-const app = express()
-app.use(express.json())
-app.use(router)
+const app = express();
+app.use(express.json());
+app.use(router);
 
 if (envs.STAGE === STAGE.TEST) {
   app.listen(3000, () => {
-    console.log("Server is running on port 3000 🚀")
-  })
+    console.log("Server is running on port 3000 🚀");
+  });
 } else {
-  module.exports.handler = ServerlessHttp(app)
+  module.exports.handler = ServerlessHttp(app);
 }
