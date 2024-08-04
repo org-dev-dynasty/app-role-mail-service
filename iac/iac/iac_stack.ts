45f9de1b2fae8bb1853eb5f190ae7a13a6ad84bc
lucapinheiro <lucapgomes11@gmail.com>
2024-08-04 01:29:07 -0300

    refactor: Update IacStack to use environment variables for configuration

--- iac/iac/iac_stack.ts
+++ iac/iac/iac_stack.ts
@@ -2,15 +2,22 @@ import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { stage } from "../get_stage_app";
 import { LambdaStack } from "./lambda_stack";
+import { envs } from "../../src/envs"; // Importe envs para obter as variáveis de ambiente
 
 export class IacStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const environments = {
-      'STAGE': stage
-    }
+    const environmentVariables = {
+      STACK_NAME: envs.STACK_NAME,
+      EMAIL_LOGIN: process.env.EMAIL_LOGIN!,
+      EMAIL_PASSWORD: process.env.EMAIL_PASSWORD!,
+      AWS_REGION: process.env.AWS_REGION!,
+      AWS_ACCOUNT_ID: process.env.AWS_ACCOUNT_ID!,
+      GITHUB_REF: process.env.GITHUB_REF!,
+      STAGE: stage,
+    };
 
-    new LambdaStack(this, environments)
+    new LambdaStack(this, environmentVariables);
   }
 }
